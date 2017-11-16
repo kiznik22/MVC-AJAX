e89c7294df6cdeefde3f756eaf2197c877d83640
unknown <gonden2159@gmail.comkiznik22>
2017-11-16 19:03:34 +0300

    release v1

---
+++ WebMy/WebMy/Controllers/AjaxController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMy.Controllers
+{
+    public class AjaxController : Controller
+    {
+        //
+        // GET: /Ajax/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ WebMy/WebMy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMy.Startup))]
+namespace WebMy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
